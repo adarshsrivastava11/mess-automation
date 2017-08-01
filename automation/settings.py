50845da582beec225ddd21c5c0664751bddb7ab4
Adarsh Srivastava <adarshs@iitk.ac.in>
2017-08-01 19:31:18 +0530

    Removed Webmail Username and Password

--- automation/settings.py
+++ automation/settings.py
@@ -117,8 +117,8 @@ EMAIL_HOST = 'mmtp.iitk.ac.in'
 EMAIL_PORT = 25
 
 # Optional SMTP authentication information for EMAIL_HOST.
-EMAIL_HOST_USER = 'adarshs'
-EMAIL_HOST_PASSWORD = 'adarsh11'
+EMAIL_HOST_USER = '<usrname>'
+EMAIL_HOST_PASSWORD = '<password>'
 EMAIL_USE_TLS = True
 
 
